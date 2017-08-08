afea7b218e88ae37911d8c56c7ec3fccab416389
coderdaduat <coderdaudat@gmail.com>
2017-08-08 22:09:24 +0700

    create api for scheduler job

---
+++ src/main/java/com/ethan/morephone/twilio/SchedulerService.java
@@ -0,0 +1,20 @@
+package com.ethan.morephone.twilio;
+
+import com.ethan.morephone.utils.Utils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by truongnguyen on 8/8/17.
+ */
+@RestController
+@RequestMapping(value = "/api/v1/scheduler")
+public class SchedulerService {
+
+    @PostMapping(value = "/job")
+    public String scheduler() {
+        Utils.logMessage("OK ROI ");
+        return "OKAY";
+    }
+}
